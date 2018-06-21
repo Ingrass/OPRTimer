bd3f67db77eafb78904d6c77ce8b15f74f5d09c2
lokpro <lok@abc>
2018-06-21 17:50:30 +0800

    Initial commit

---
+++ OPRTimer.user.js
@@ -0,0 +1,42 @@
+﻿// ==UserScript==
+// @name         OPRTimer
+// @version      1.0
+// @category     Info
+// @namespace    sdf12321asccerverv
+// @author       lokpro
+// @match        https://opr.ingress.com/recon*
+// @grant        none
+// ==/UserScript==
+
+// CountingTimer
+
+var interval = setInterval( function(){
+	
+	// wait for information available
+	var expires;
+	try {
+		expires = angular.element(document.getElementById('NewSubmissionController')).scope().subCtrl.pageData.expires;
+	} catch (e) {
+		return;
+	}
+	// information OK
+	clearInterval( interval );
+
+	document.querySelector(  '.navbar-header>a').innerHTML += "&nbsp;&nbsp;<span id='CountingTimer' class1='navbar-brand' style='float:right; color:#FFFF00;'></span>";
+	var countingTimer = document.getElementById('CountingTimer');
+
+	var date0 = new Date( expires );
+
+	var timer = setInterval( function(){
+		var date = date0 - new Date();
+		
+		if( date<=0 ){
+			date = 0;
+			clearInterval( timer );
+		}
+		
+		countingTimer.innerText = new Date(date).toISOString().substr(14, 5);
+	}, 1000 );
+}, 99 );
+
+
---
+++ README.md
@@ -0,0 +1,13 @@
+## #OPRscript
+## 審po時間倒計時 OPR CountDown Timer
+
+## script url:
+https://github.com/Ingrass/OPRTimer/raw/master/OPRTimer.js
+
+![readme](readme1.png)
+
+在使用時，請確定手機或電腦本機的時間準確。
+Please ensure the accuracy of the time on the location machine.
+
+檢查標準時間網站 / Time check:
+https://time.is/
